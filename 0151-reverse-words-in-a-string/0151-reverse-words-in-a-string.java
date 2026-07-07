a0554e030abba39f44fd53f3d86bffa22749bd4d
Ronak Tailor <tailorronak6@gmail.com>
2026-07-07 16:46:04 +0530

    Time: 8 ms (36.32%), Space: 44.7 MB (31.93%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.split("\\s+"); // split ont the basis of spaces
+
+        StringBuilder sb = new StringBuilder("");
+
+        for(int i=arr.length-1; i>=0; i--){
+            sb.append(arr[i] + " ");
+        }
+
+        // trim function --> remove trailing and leading spaces
+
+        String ans = sb.toString();
+
+        ans = ans.trim();
+
+        return ans;
+        
+    }
+}
