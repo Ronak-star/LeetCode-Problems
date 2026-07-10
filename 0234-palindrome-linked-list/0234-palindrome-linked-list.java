5b67821a21650de8ad2dadf08ff0f69b3ee66605
Ronak Tailor <tailorronak6@gmail.com>
2026-07-10 14:37:09 +0530

    Time: 16 ms (5.73%), Space: 101.5 MB (10.74%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -8,6 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+ /*
 class Solution {
    public ListNode getMiddle(ListNode head) {
    ListNode fast = head;
@@ -56,4 +57,34 @@ public boolean isPalindrome(ListNode head) {
    return true;
 }
  
+}
+
+*/
+
+
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        Stack<Integer> st = new Stack<>();
+
+        ListNode ptr = head ;
+
+        while(ptr!=null){
+            st.push(ptr.val);
+            ptr = ptr.next;
+        }
+
+        ListNode ptr2 = head ;
+
+        while(ptr2!=null){
+            int val1 = ptr2.val;
+            int val2 = st.pop();
+
+            if(val1 != val2 ){
+                return false ;
+            }
+            ptr2 = ptr2.next;
+        }
+
+        return true ;
+    }
 }
