d38578eada8ef4edb5d21a29b0d96dfd77534090
Ronak Tailor <tailorronak6@gmail.com>
2026-07-29 14:11:48 +0530

    Time: 6 ms (52.62%), Space: 7.7 MB (65.53%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int fib(int n) {
+        /*
        if(n==0){
             return 0;
         }
@@ -18,5 +19,12 @@ public:
             Secondterm = Thirdterm;
         }
         return Firstterm;
+    }
+    */
+
+    if(n<=1)
+    return n;
+
+return fib(n-1)+fib(n-2);
     }
 };
