fa793559bc95bbc09e8db7f364e18307bc3f4466
Ronak Tailor <tailorronak6@gmail.com>
2026-07-29 15:34:24 +0530

    Time: 3 ms (27.5%), Space: 15.8 MB (5.8%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,6 +1,26 @@
 class Solution {
 public:
+vector<vector<int>> arr;
+
+void helper(int i,vector<int>& nums,vector<int> ans){
+    if(i==nums.size()){
+        arr.push_back(ans);
+        return; //Base condition
+    }
+
+    // include
+    ans.push_back(nums[i]);
+    helper(i+1, nums, ans);
+    //backtraking
+    ans.pop_back();
+    // exclude
+    helper(i+1,nums, ans);
+}
     vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>ans;
+        helper(0,nums,ans);
+        return arr;
+        /*
         vector<vector<int>> res = {{}};
         for (int num : nums) {
             int n = res.size();
@@ -11,5 +31,37 @@ public:
             }
         }
         return res;
+        */
+
+
+/*
+        vector<vector<int>> res;
+        vector<int> path;
+        solve(nums, 0, path, res);
+        return res;
+    }
+    
+private:
+    void solve(vector<int>& nums, int index, vector<int>& path, vector<vector<int>>& res) {
+        // Base case: processed all elements, record this subset
+        if (index == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+        
+        // Choice 1: exclude nums[index]
+        solve(nums, index + 1, path, res);
+        
+        // Choice 2: include nums[index]
+        path.push_back(nums[index]);
+        solve(nums, index + 1, path, res);
+        path.pop_back(); // backtrack
+*/
+
+
+
+
+
+
     }
 };
